7c89e6b4f1f03f94eb8a31190b6d3be45362b79f
Fengyarnom <Fengyarnom@gmail.com>
2022-11-06 16:27:22 +0800

    :zap: 路由初步
    初步完成了后端路由，由于没有使用React等JS库，所以这一部分做起来还是蛮难受的，前端的代耦合在一切，但是由于这是一个小的项目，为了能够快速运行，索性还是抛弃了React。

--- core/views/index.py
+++ core/views/index.py
@@ -3,7 +3,12 @@ from flask import Blueprint, render_template
 bp = Blueprint('index', __name__, url_prefix='/')
 
 @bp.route("/")
-@bp.route("/a")
-def hello_world(name=None):
+@bp.route("/article")
+def index(name=None):
 
     return render_template("article.html",name=name)
+
+@bp.route("/daily")
+def daily(name=None):
+
+    return render_template("daily.html",name=name)
